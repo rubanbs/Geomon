ec7d6742dc6a3cb9817711f1bf41d5cd780dec84
Boris Ruban <rubanbsgit@gmail.com>
2015-01-06 23:28:31 +0300

    When two points too close to each other - remove previos point from track. This allows to keep track clean without redundant coordinates on the map.

--- config/database.js
+++ config/database.js
@@ -7,6 +7,9 @@
     //  trackSize : 20 -- Only 20 last coordinates saved in DB
     //  trackSize : 0  -- No history saved
     //  trackSize : -1 -- All history saved
-    trackSize : 25
+    trackSize : 25,
+	
+	// Proximity of two points in meters. When two points too close to each other - remove previos point from track
+	proximity : 10
 
 };
